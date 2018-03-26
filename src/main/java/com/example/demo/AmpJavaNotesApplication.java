bb2eb2f9d27de65faa439f87f830dc659c1219c0
amp8293 <amp8293@gmail.com>
2018-03-26 17:07:22 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/AmpJavaNotesApplication.java
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ */
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpJavaNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpJavaNotesApplication.class, args);
+	}
+}
